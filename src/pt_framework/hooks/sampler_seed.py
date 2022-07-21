2b35c78379f72e5fadae36cb2742b274150fe806
Chengxu Zhuang <zhuangcx93@gmail.com>
2022-07-21 18:05:31 +0000

    minor fix

--- src/pt_framework/hooks/sampler_seed.py
+++ src/pt_framework/hooks/sampler_seed.py
@@ -4,7 +4,7 @@ from ..dist_utils import use_tpu
 
 class DistSamplerSeedHook(Hook):
     def before_epoch(self, runner):
-        if use_tpu:
+        if use_tpu():
             loader = runner.data_loader._loader
         else:
             loader = runner.data_loader
